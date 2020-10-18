151370f92e48856f6c1a160937d3230d92a01c08
Vinod Shalgar <vinod.shalgar@oneadvanced.com>
2020-10-18 20:49:25 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount = async () => {
+    const response = await axios.get("/todo/list");
+    this.setState({
+      todos: response.data.todos
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My Todo List</h1>
+        <header className="App-header">
+          {this.state.todos.map(todo => (
+            <h2>{todo}</h2>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
